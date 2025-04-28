bc19dab7be8ece0cec6ecbfeb4e799f44d31eac3
bielbarros <gabrielbarrosg11@gmail.com>
2025-04-27 21:27:08 -0300

    feat: Update notification repository

--- magalums/src/main/java/com/gerenciador/magalums/repository/NotificationRepository.java
+++ magalums/src/main/java/com/gerenciador/magalums/repository/NotificationRepository.java
@@ -1,8 +1,13 @@
 package com.gerenciador.magalums.repository;
 
 import com.gerenciador.magalums.entiity.Notification;
+import com.gerenciador.magalums.entiity.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByStatusInAndDateTimeBefore(List<Status> status, LocalDateTime dateTime);
 
 }
